ecc32a8b7a0219334708fdbf2f7c9dbb80ed3c55
Maxik-svg <60792664+Maxik-svg@users.noreply.github.com>
2020-05-29 22:24:15 +0300

    1

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = "mongodb+srv://kolomoitsev:jMBueTbBymeolFAU@cluster0-hnn0k.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
