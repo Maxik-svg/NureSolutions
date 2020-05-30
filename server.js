94b583e2a8fa1aa4cb398d0e5da9b3b40170b9ee
Maxik-svg <60792664+Maxik-svg@users.noreply.github.com>
2020-05-30 17:29:45 +0300

    mongo

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb://heroku_6p94bwm3:q4ibs5di3hgb1v4m31nf4idofu@ds261114.mlab.com:61114/heroku_6p94bwm3"; 
+const uri = "mongodb+srv://kolomoitsev:jMBueTbBymeolFAU@cluster0-hnn0k.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
