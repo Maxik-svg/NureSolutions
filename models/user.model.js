6f8208866bfa73a680431bdad3c6382cb3908b3e
Maxik-svg <60792664+Maxik-svg@users.noreply.github.com>
2020-05-31 12:43:03 +0300

    a

--- models/user.model.js
+++ models/user.model.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
             minlength: 3
         },
         image:{
-            type: File
+            type: String
         },
         login: {
             type: String,
